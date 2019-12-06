afd43e38bac3823f729dc8761a5fbae7c5f58b97
Alexsander Ramos da Silva <alexsanderramosdasilva@alexsander-mac.local>
2019-12-06 00:23:52 -0300

    app created

---
+++ src/app/Player.ts
@@ -0,0 +1,10 @@
+export class Player {
+    score: number;
+    advantages: number;
+    punitions: number;
+    constructor() {
+        this.score = 0;
+        this.advantages = 0;
+        this.punitions = 0
+    }
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,66 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Player } from '../Player';
+import { ElectronService } from '../core/services';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
+
 export class HomeComponent implements OnInit {
+  titulo: string;
+  white: Player;
+  green: Player;
+  classes: Object;
 
-  constructor() { }
+  constructor(private electronService: ElectronService) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.titulo = "ScoreMark";
+    this.white = new Player();
+    this.green = new Player();
+    this.classes = {
+      "whiteScore": "",
+      "greenScore": "",
+      "greenPunition": "",
+      "greenAdvantage": "",
+      "whiteAdvantage": "",
+      "whitePunition": ""
+    }
+  }
 
+  fullScreen(): void {
+    let win = this.electronService.remote.getCurrentWindow();
+    win.setFullScreen(!win.isFullScreen());
+  }
+
+  handleClick(oEvent) {
+    let id = this.getId(oEvent);
+    this.classes[id] = (this.classes[id] === "" ? "blink_me" : "");
+
+    switch (id) {
+      case "whiteScore":
+        break;
+      case "greenScore":
+        break;
+      case "greenPunition":
+        break;
+      case "greenAdvantage":
+        break;
+      case "whiteAdvantage":
+        break;
+      case "whitePunition":
+        break;
+      default:
+        break;
+    }
+  }
+  private getId(oEvent): string {
+    return oEvent.target.attributes["id"].value;
+  }
+
+  private getClass(id): string{
+    return this.classes[id];
+  }
 }
